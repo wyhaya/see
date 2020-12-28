c39e37b6206b7edecdd283bbb70f54198cccadb6
wyhaya <wyhaya@gmail.com>
2020-12-28 19:45:13 +0800

    Fix body compress

--- src/body.rs
+++ src/body.rs
@@ -1,56 +1,62 @@
 use crate::compress::Encoding;
 use crate::default;
-use async_compression::stream::{BrotliEncoder, DeflateEncoder, GzipEncoder};
-use futures_util::stream::{self, StreamExt};
-use hyper::body::{Body, Bytes};
+use async_compression::tokio::bufread::{BrotliEncoder, DeflateEncoder, GzipEncoder};
+use hyper::body::Body;
+use std::io::Cursor;
 use tokio::fs::File;
+use tokio::io::BufReader;
 use tokio_util::codec::{BytesCodec, FramedRead};
 
 pub struct BodyStream {
     compress: Option<Encoding>,
 }
 
-// TODO: https://github.com/Nemo157/async-compression/pull/107
 impl BodyStream {
     pub fn new(compress: Option<Encoding>) -> Self {
         Self { compress }
     }
 
     pub fn file(self, file: File) -> Body {
-        // let file = FramedRead::with_capacity(file, BytesCodec::new(), default::BUF_SIZE)
-        //     .map(|rst| rst.map(|bytes| bytes.freeze()));
-
-        // match self.compress {
-        //     Some(mode) => match mode {
-        //         Encoding::Gzip(level) => Body::wrap_stream(GzipEncoder::with_quality(file, level)),
-        //         Encoding::Br(level) => Body::wrap_stream(BrotliEncoder::with_quality(file, level)),
-        //         Encoding::Deflate(level) => {
-        //             Body::wrap_stream(DeflateEncoder::with_quality(file, level))
-        //         }
-        //     },
-        //     None => Body::wrap_stream(file),
-        // }
-        todo!()
+        let file = BufReader::with_capacity(default::BUF_SIZE, file);
+        match self.compress {
+            Some(encoding) => match encoding {
+                Encoding::Gzip(level) => Body::wrap_stream(FramedRead::new(
+                    GzipEncoder::with_quality(file, level),
+                    BytesCodec::new(),
+                )),
+                Encoding::Br(level) => Body::wrap_stream(FramedRead::new(
+                    BrotliEncoder::with_quality(file, level),
+                    BytesCodec::new(),
+                )),
+                Encoding::Deflate(level) => Body::wrap_stream(FramedRead::new(
+                    DeflateEncoder::with_quality(file, level),
+                    BytesCodec::new(),
+                )),
+            },
+            None => Body::wrap_stream(FramedRead::new(file, BytesCodec::new())),
+        }
     }
 
     pub fn text(self, text: String) -> Body {
-        // match self.compress {
-        //     Some(mode) => {
-        //         let text = stream::once(async move { Ok(Bytes::from(text)) });
-        //         match mode {
-        //             Encoding::Gzip(level) => {
-        //                 Body::wrap_stream(GzipEncoder::with_quality(text, level))
-        //             }
-        //             Encoding::Br(level) => {
-        //                 Body::wrap_stream(BrotliEncoder::with_quality(text, level))
-        //             }
-        //             Encoding::Deflate(level) => {
-        //                 Body::wrap_stream(DeflateEncoder::with_quality(text, level))
-        //             }
-        //         }
-        //     }
-        //     None => Body::from(text),
-        // }
-        todo!()
+        match self.compress {
+            Some(encoding) => {
+                let data = Cursor::new(text.into_bytes());
+                match encoding {
+                    Encoding::Gzip(level) => Body::wrap_stream(FramedRead::new(
+                        GzipEncoder::with_quality(data, level),
+                        BytesCodec::new(),
+                    )),
+                    Encoding::Br(level) => Body::wrap_stream(FramedRead::new(
+                        BrotliEncoder::with_quality(data, level),
+                        BytesCodec::new(),
+                    )),
+                    Encoding::Deflate(level) => Body::wrap_stream(FramedRead::new(
+                        DeflateEncoder::with_quality(data, level),
+                        BytesCodec::new(),
+                    )),
+                }
+            }
+            None => Body::from(text),
+        }
     }
 }
